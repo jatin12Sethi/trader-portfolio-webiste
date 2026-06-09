449812bbc18e15713a19dc53bf77fc8bbafdee35
Jatin Sethi <jatin12sethi@gmail.com>
2026-06-09 08:50:53 +0530

    chore: add config.php for SMTP mail configuration

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Configuration File
+// DO NOT COMMIT THIS FILE TO GITHUB ONCE IT HAS REAL PASSWORDS!
+
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 465); // SSL
+define('SMTP_USER', 'Sethijatin928@gmail.com'); // Replace with your actual email
+define('SMTP_PASS', 'clcq jbyf yzsr kjki'); // Replace with App Password
+?>
